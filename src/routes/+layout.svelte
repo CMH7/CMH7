e6953ce7e1f3ed1ee5214c4d47a796d21e9e5a70
cmheyselnat300713 <charlesMaverickHerrera@plv.edu.ph>
2023-01-04 20:01:20 +0800

    [ADD] Tailwind with dark mode option

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+  import { isDarkMode } from "$lib/stores/global.store";
+</script>
+
+<div class="{$isDarkMode ? 'dark' : '' }">
+  <div class="text-black dark:text-white p-5 bg-slate-600 dark:bg-slate-900">
+    Hello
+  </div>
+</div>
+
+<slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { isDarkMode } from "$lib/stores/global.store";
+</script>
+
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+  on:click={() => {
+    if(!$isDarkMode) {
+      isDarkMode.set(true)
+    } else {
+      isDarkMode.set(false)
+    }
+  }}
+>
+  change
+</div>
